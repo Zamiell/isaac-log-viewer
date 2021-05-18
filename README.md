69a6b1b61a12249b52eec3530519c0156b5b996e
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-05-18 11:05:53 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # isaac-log-viewer
+
+A real time viewer for the log.txt file of *The Binding of Isaac: Repentance*.
---
+++ main.py
@@ -0,0 +1,74 @@
+# Imports
+import os
+import sys
+import time
+
+# Constants
+LOG_FILE_PATH = (
+    "C:\\Users\\james\\Documents\\My Games\\Binding of Isaac Repentance\\log.txt"
+)
+
+# Variables
+log_file_handle = None
+cached_length = 0
+
+
+def main():
+    while True:
+        file_size = os.path.getsize(LOG_FILE_PATH)
+        if has_log_changed(file_size):
+            read_log(file_size)
+        time.sleep(0.1)
+
+
+def has_log_changed(file_size: int):
+    return file_size != cached_length
+
+
+def read_log(file_size: int):
+    global log_file_handle
+    global cached_length
+
+    if log_file_handle is None or cached_length > file_size:
+        # This is a new log file
+        log_file_handle = open(LOG_FILE_PATH, "rb")
+    elif cached_length < file_size:
+        # Append existing content
+        log_file_handle.seek(cached_length)
+
+    cached_length = file_size
+    new_log_content = log_file_handle.read()
+    parse_log(new_log_content)
+
+
+def parse_log(log_content: str):
+    for line in log_content.splitlines():
+        parse_log_line(line)
+
+
+def parse_log_line(line_bytes: str):
+    # We read the log in binary form, so we need to convert it to a normal string
+    line = line_bytes.decode("utf-8")
+
+    # Don't print empty lines
+    if line == "":
+        return
+
+    # Print all warnings and errors
+    lowercase_line = line.lower()
+    if "warn" in lowercase_line or "error" in lowercase_line:
+        printf(line)
+        return
+
+    # Print lines that have to do with Lua
+    if "lua" in lowercase_line:
+        printf(line)
+        return
+
+
+def printf(msg: str):
+    print(msg, flush=True)
+
+
+if __name__ == "__main__":
+    main()
